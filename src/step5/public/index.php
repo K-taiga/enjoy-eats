4374c8aa4123f7d6f3850ae81d8dc578538009f7
K-taiga <taigakatou0329@gmail.com>
2021-05-04 15:57:04 +0900

    finish step5

--- src/step5/public/index.php
+++ src/step5/public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// クロージャーを受け取り直後にその関数を実行する
 $handleException = require __DIR__ . '/../app/Core/handle-exception.php';
 $handleException();
 
